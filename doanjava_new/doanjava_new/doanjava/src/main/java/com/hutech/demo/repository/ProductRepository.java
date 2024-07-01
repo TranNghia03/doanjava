b7fd45f3d44f0c88843988a3112f0a350cbd08da
vogiabao123 <146648388+vogiabao123@users.noreply.github.com>
2024-07-01 17:04:32 +0700

    vohieuhoasanpham

--- doanjava_new/doanjava_new/doanjava/src/main/java/com/hutech/demo/repository/ProductRepository.java
+++ doanjava_new/doanjava_new/doanjava/src/main/java/com/hutech/demo/repository/ProductRepository.java
@@ -3,9 +3,11 @@ package com.hutech.demo.repository;
 import com.hutech.demo.models.Category;
 import com.hutech.demo.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -13,4 +15,15 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategory(Category category);
     List<Product> findAllByOrderByPriceAsc();
     List<Product> findAllByOrderByPriceDesc();
+    @Query("SELECT p FROM Product p WHERE p.isDelete = false")
+    List<Product> findAllNotDeleted();
+
+    @Query("SELECT p FROM Product p WHERE p.id = :id AND p.isDelete = false")
+    Optional<Product> findByIdAndNotDeleted(Long id);
+
+    @Query("SELECT p FROM Product p WHERE p.isDelete = true")
+    List<Product> findAllDeleted();
+
+    @Query("SELECT p FROM Product p WHERE p.isActive = true AND p.isDelete = false")
+    List<Product> findAllActiveAndNotDeleted();
 }
